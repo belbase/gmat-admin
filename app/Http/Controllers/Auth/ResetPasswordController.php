65365bdd8127546dc81d914749f86e635b216883
Deepak Belbase <webmaster@edushastra.online>
2018-01-17 17:13:32 +0530

    Modified: Reset Password Alert Setting!

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -38,10 +38,31 @@ class ResetPasswordController extends Controller
         $this->middleware('guest');
     }
 
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function showResetForm(Request $request, $token = null)
     {
         return view('auth.passwords.reset')->with(
             ['token' => $token, 'email' => $request->email, 'title'=>'Reset Your Account Password']
         );
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse($response)
+    {
+        return redirect($this->redirectPath())
+                            ->with('success', trans($response));
     }
 }
